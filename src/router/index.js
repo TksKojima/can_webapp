f31dde3d5656671a16be333ed6ba5964dd5d2b1f
kojimaHome <kt>
2023-01-24 07:45:19 +0900

    VueCan

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/CanRecv.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'can_recv',
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/can_recv',
+    name: 'can_recv2',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
